15a3652f78b3e3fb4fda274e9fd8c84bef20aa07
Noor Ullah <155673480+NoorUllah43@users.noreply.github.com>
2024-02-27 22:45:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const container = document.querySelector('.container');
 const input = document.querySelector('.form-control');
 const btn = document.querySelector('#button-addon2');
-const addmoreBtn = document.querySelector('#addmoreBtn');
+const paginationBox = document.querySelector(".pagination-box");
+const loading = document.querySelector(".loading");
+
+console.log(container);
+console.log(container);
 
 
-async function main(a=1) {
+async function main(a = 1) {
 
-    let url = `https://api.unsplash.com/search/photos?page=${a}&query=${input.value}&client_id=6aRKI784yJ2OTfT5b7kwljlA6Hsm9CCHEO4Kyttz2R0`;
-    
-    let data = await fetch(url);
-    let result = await data.json();
-    console.log(result);
+    container.innerHTML = '';
+    loading.classList.remove('d-none')
 
-    for(let i = 0; i < 10; i++){
-    
-        container.innerHTML += `<div class="text-center">
-        <img src="${result.results[i].urls.full}" class="rounded box" alt="...">
-      </div>`
+    try {
+        let url = `https://api.unsplash.com/search/photos?page=${a}&query=${input.value}&client_id=6aRKI784yJ2OTfT5b7kwljlA6Hsm9CCHEO4Kyttz2R0`;
+
+        let data = await fetch(url);
+        var result = await data.json();
+        console.log(result);
+    }
+    catch (error) {
+        alert('check spelling')
+    }
+    for (let i = 0; i < 10; i++) {
+
+        container.innerHTML += `<img src="${result.results[i].urls.full}" class="img-fluid rounded m-2 " alt="...">`
     }
 
-    addmoreBtn.style.display = 'block';
+    loading.classList.add('d-none')
+    paginationBox.classList.remove("d-none");
+
+
 
 }
 function searchFun() {
@@ -27,20 +39,36 @@ function searchFun() {
     main()
 }
 
-let page = 2;
-function addmore() {
+
+function addmore(page) {
     main(page);
-    page++;
+
+}
+
+let page = 4;
+function nextpage(e) {
+    if (e.innerHTML == `»`) {
+        main(page);
+        page++;
+
+    }
+    else if (e.innerHTML == `«`) {
+
+        page--;
+        main(page);
+
+    }
+
+    
 }
 
 
-btn.addEventListener('click',searchFun);
-addmoreBtn.addEventListener('click',addmore)
-document.addEventListener('keyup',(key) => {
-    if(key.key == 'Enter'){
-        searchFun();
+btn.addEventListener('click', main);
+document.addEventListener('keyup', (key) => {
+    if (key.key == 'Enter') {
+        main();
     }
-  
+
 }
 )
 
